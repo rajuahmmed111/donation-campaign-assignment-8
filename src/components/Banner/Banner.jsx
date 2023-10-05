241762de3702291b95fe98db240c41e27fe22d03
rajuahmmed111 <ahraju2003@gmail.com>
2023-10-05 17:32:03 +0600

    added the last work

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,12 @@
 
 const Banner = () => {
-
-    const [searchValue, setSearchValue] = ([])
-
-    const handleSearch = (value) => {
-        const isExist = searchValue.filter(item => item.id == value.id)
-        setSearchValue(isExist);
-    }
-        
-
-
+    
     return (
         <div className="mt-28" >
             <h2 className='text-4xl font-bold text-center'>I Grow By Helping People In Need</h2>
             <div className="w-[410px] h-[150px] lg:ml-96 mt-8 ml-20">
                 <input className="border-2 w-80 h-12 p-3" type="search" placeholder="Search here..." />
-                <button onSubmit={() => handleSearch(searchValue)} className="btn btn-error">Search</button>
+                <button  className="btn btn-error">Search</button>
             </div>
         </div>
     );
